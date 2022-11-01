9497e1ef8086aa427d7d86b438be20d682f85cc6
nk-akun <nk_akun@163.com>
2022-11-01 15:40:16 +0800

    feat: add drivercc 2,3,4,5

--- blockchain-trace-bcnetwork/webapp/startDriverCC.sh
+++ blockchain-trace-bcnetwork/webapp/startDriverCC.sh
@@ -8,7 +8,7 @@ docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/g
 
 docker exec -e "CORE_PEER_LOCALMSPID=Org2MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.trace.com/users/Admin@org2.trace.com/msp" -e "CORE_PEER_ADDRESS=peer0.org2.trace.com:7051" cli peer chaincode install -n drivercc -v 1.0 -l golang -p github.com/chaincode/drivercc/go/
 
-docker exec -e "CORE_PEER_LOCALMSPID=Org3MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org4.trace.com/users/Admin@org3.trace.com/msp" -e "CORE_PEER_ADDRESS=peer0.org3.trace.com:7051" cli peer chaincode install -n drivercc -v 1.0 -l golang -p github.com/chaincode/drivercc/go/
+docker exec -e "CORE_PEER_LOCALMSPID=Org3MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org3.trace.com/users/Admin@org3.trace.com/msp" -e "CORE_PEER_ADDRESS=peer0.org3.trace.com:7051" cli peer chaincode install -n drivercc -v 1.0 -l golang -p github.com/chaincode/drivercc/go/
 
 docker exec -e "CORE_PEER_LOCALMSPID=Org4MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org4.trace.com/users/Admin@org4.trace.com/msp" -e "CORE_PEER_ADDRESS=peer0.org4.trace.com:7051" cli peer chaincode install -n drivercc -v 1.0 -l golang -p github.com/chaincode/drivercc/go/
 
